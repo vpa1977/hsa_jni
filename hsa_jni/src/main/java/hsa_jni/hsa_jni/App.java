b134c53f1dff7496d8c8ecf6edb7f4075bf1d861
bsp <vpa1977@gmail.com>
2014-10-30 23:07:21 +1300

    .

---
+++ hsa_jni/src/main/java/hsa_jni/hsa_jni/App.java
@@ -0,0 +1,135 @@
+package hsa_jni.hsa_jni;
+
+import hsa_jni.hsa_jni.WekaHSAContext.KnnNativeContext;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException, NoSuchMethodException, SecurityException
+    {
+    	
+		
+    	System.out.println(System.getProperty("java.class.path"));
+
+    	System.out.println(System.getProperty("java.class.path"));
+    	ArffLoader loader = new ArffLoader();
+    	loader.reset();
+    	loader.setSource(new File("electNormNew.arff"));
+		Instances instances = loader.getStructure();
+    	System.out.println("----");
+    	
+    	
+    	WekaHSAContext context = new WekaHSAContext();
+    	
+    	KnnNativeContext knn = context.create( instances, 8192);
+    	Instance inst;
+    	int count = 0;
+    	Instance test = loader.getNextInstance(instances);
+		while ( (inst = loader.getNextInstance(instances)) != null)
+		{
+			System.out.println("Adding "+(++count));
+			knn.addInstance(inst);
+		}
+		for (int i = 0 ;i < 1000 ;  ++i)
+			knn.computeKnn(test);
+		
+		System.out.println("Check ranges");
+		
+		long start = System.currentTimeMillis();
+		for (int i = 0 ;i < 1000 ;  ++i)
+			knn.computeKnn(test);
+		long end = System.currentTimeMillis();
+    	System.out.println("Time "+ (end -start));
+		
+		
+		
+    	/*ArrayList<Attribute> attinfo = new ArrayList<Attribute>();
+		attinfo.add( new Attribute("1"));
+		attinfo.add( new Attribute("2"));
+		attinfo.add( new Attribute("3"));
+		attinfo.add( new Attribute("4"));
+		attinfo.add( new Attribute("class"));
+		
+		Instances dataset = new Instances( "test", attinfo , 1);
+		dataset.setClassIndex(0);
+
+    
+    	
+    	WekaHSAContext context = new WekaHSAContext();
+    	
+    	KnnNativeContext knn = context.create( dataset, 16);
+    	
+    	for (int i = 0 ; i < 32; ++i)
+    	{
+    		Instance inst = new DenseInstance(1, new double[]{ 1, 1, 1 , 1 ,1});
+    		inst.setDataset(dataset);
+    		knn.addInstance(inst);
+    	}
+    	
+    	
+    	
+		Instance inst = new DenseInstance(1, new double[]{ 0, 0, 0 , 0 ,0});
+		inst.setDataset(dataset);
+    	
+		knn.computeKnn(inst);*/
+    	
+    	
+    	  
+    	 
+    	/*
+    	int count = 0;
+    	System.out.println(count);
+    	
+    	Iterator<Instance> it = data.iterator();
+    	while (it.hasNext())
+    	{
+    		context.AddInstance(it.next());
+    		context.AddPureJavaInstance(it.next());
+    	}
+    		
+    	context.dispose();
+    	context.disposePureJava();
+    	
+    	long start = System.nanoTime();
+    	it = data.iterator();
+    	for (int i = 0; i < 4*8192 ; ++i)
+    	{
+    		context.AddInstance(it.next());
+    	}
+    	
+    	context.dispose();
+    	long end = System.nanoTime();
+    	
+    	System.out.println("Time "+ (end -start)/1000000);
+    	
+    	
+    	start = System.nanoTime();
+    	it = data.iterator();
+    	for (int i = 0; i < 4*8192 ; ++i)
+    		context.AddPureJavaInstance(it.next());
+    	context.commit();
+    	context.disposePureJava();
+    	end = System.nanoTime();
+    	
+    	System.out.println("Time2 "+ (end -start)/1000000); 
+    	*/
+    	
+    	
+
+    }
+}
---
+++ hsa_jni/src/main/java/hsa_jni/hsa_jni/WekaHSAContext.java
@@ -0,0 +1,156 @@
+package hsa_jni.hsa_jni;
+
+import java.util.ArrayList;
+
+import weka.core.Attribute;
+import weka.core.Instance;
+import weka.core.Instances;
+
+/** 
+ * 
+ * Provides JNI interface to the 
+ * instance data 
+ *
+ */
+public class WekaHSAContext {
+	
+	static {
+		System.loadLibrary("weka_jni");
+	}
+	
+	KnnNativeContext m_context;
+	
+	public WekaHSAContext() {
+		init();		
+	}
+	
+	
+	public WekaHSAContext.KnnNativeContext create(Instances model, int size)
+	{
+		return new KnnNativeContext(model, size);
+	}
+	
+	
+	public class KnnNativeContext
+	{
+		public KnnNativeContext(Instances model, int size)
+		{
+			m_model = model;
+			m_instances = new Instance[size];
+			
+			ArrayList<Integer> nominals = new  ArrayList<Integer>();
+			ArrayList<Integer> numerics = new ArrayList<Integer>();
+
+			int class_index = m_model.classIndex();
+			for (int i = 0 ; i < m_model.numAttributes() ; i ++ )
+			{
+				if (i == class_index)
+					continue;
+				
+				Attribute attr = m_model.attribute(i);
+				if (attr.isNominal())
+					nominals.add(i);
+				if (attr.isNumeric())
+					numerics.add(i);
+			}
+			m_numerics = numerics.toArray(new Integer[numerics.size()]);
+			m_nominals = nominals.toArray(new Integer[nominals.size()]);
+			m_window = new double[ size * (m_numerics.length + m_nominals.length)];
+			m_ranges = new double[ m_numerics.length * 2];
+			m_tmp_distances = new double [size];
+			m_tmp_indexes = new int[size];
+			
+			for (int i = 0 ;i < m_numerics.length; ++i)
+			{
+				m_ranges[i*2] = Double.MAX_VALUE;
+				m_ranges[i*2 +1]= Double.MIN_VALUE;
+			}
+			m_index = 0;
+			m_current_size = 0;
+		}
+		
+		public void computeKnn(Instance test)
+		{
+			double [] testArr = new double[m_nominals.length + m_numerics.length];
+			int offset = 0;
+			double[] ranges = new double[ m_ranges.length];
+			System.arraycopy(m_ranges, 0, ranges, 0, ranges.length);
+			for (int i = 0; i < m_numerics.length; i ++ ) 
+			{
+				int idx = m_numerics[i];
+				testArr[offset]  = test.value(idx);
+				if (ranges[i*2] > testArr[offset])
+					ranges[i*2] = testArr[offset];
+				else if (ranges[i*2+1] < testArr[offset])
+					ranges[i*2+1] = testArr[offset];
+				++offset;
+			}
+			
+			for (int i = 0; i < m_nominals.length; i ++ ) 
+				testArr[offset++] = test.value(m_nominals[i]);
+			
+			knn (testArr,m_window, ranges,m_numerics.length,
+					m_nominals.length + m_numerics.length, 
+					m_instances.length, 
+					m_tmp_distances, m_tmp_indexes);
+			
+		}
+		
+		public void addInstance(Instance inst)
+		{
+			boolean need_rescan = false;
+			
+			m_instances[m_index] = inst;
+			int offset = m_index * (m_nominals.length + m_numerics.length);
+			for (int i = 0; i < m_numerics.length; i ++ ) 
+			{
+				int idx = m_numerics[i];
+				double val  = m_window[offset];
+				if (m_ranges[i*2] == val || m_ranges[i*2+1] == val )
+					rescanRanges(m_window, m_ranges,m_numerics.length, m_numerics.length + m_nominals.length, m_current_size, i);
+				m_window[offset] = inst.value(idx);
+				if (m_ranges[i*2] > m_window[offset])
+					m_ranges[i*2] = m_window[offset];
+				else if (m_ranges[i*2+1] < m_window[offset])
+					m_ranges[i*2+1] = m_window[offset];
+				++offset;
+			}
+			
+			for (int i = 0; i < m_nominals.length; i ++ ) 
+			{
+				m_window[offset++] = inst.value(m_nominals[i]);
+			}
+			
+			++m_index;
+			if (m_current_size < m_instances.length)
+				++m_current_size;
+			if (m_index == m_instances.length)
+				m_index = 0;
+		}
+		
+		private native void knn(double[] instance, double[] m_window, double[] m_ranges, int numerics_size,int instance_size, int window_size, double[] result_distance, int[] result_index);
+		private native void rescanRanges(double[] m_window, double[] m_ranges, int numerics_size,int instance_size, int window_size, int attribute );
+		
+		public Instances m_model;
+		public Instance[] m_instances;
+		public double[] m_window;
+		public double[] m_ranges;
+		private Integer[] m_numerics;
+		private Integer[] m_nominals;
+		private int m_index;
+		private int m_current_size;
+		
+		private double[] m_tmp_distances;
+		private int[] m_tmp_indexes;
+	}
+	
+
+	/** 
+	 * load gpu kernels
+	 */
+	public native void init();
+	
+	
+	
+	
+}
