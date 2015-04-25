d32590faa5363fdab0774270cfa449f70053d4cd
Jonhn <vpa1977@gmail.com>
2015-04-26 10:04:36 +1200

    Squashed commit of the following:
    commit f44d2b54a65ec8c80989422d5fee830907dabaa8
    Author: vpa1977 <vpa1977@gmail.com>
    Date:   Tue Dec 23 16:06:16 2014 +1300
    
    	Delete libweka_jni.so

---
+++ viennacl-frontend/moa-frontend-lib/library.cpp
@@ -0,0 +1,12 @@
+/*
+ * MOA frontend to viennacl ml library
+ *
+ *  Created on: 24/04/2015
+ *      Author: bsp
+ */
+#include "viennacl/context.hpp"
+
+viennacl::context g_context;
+
+
+
---
+++ viennacl-frontend/moa-frontend/src/org/moa/gpu/SGD.java
@@ -0,0 +1,5 @@
+package org.moa.gpu;
+
+public class SGD {
+
+}
